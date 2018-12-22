63518ba36c0dbfbd4bca487f633725926a651d91
Varisara N <varisara.n@gmail.com>
2018-12-22 13:17:12 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
-  move = position.to_i - 1
+  move = position.to_i
   if !position_taken?(board, move) && move.between?(0,8)
     true
   else
